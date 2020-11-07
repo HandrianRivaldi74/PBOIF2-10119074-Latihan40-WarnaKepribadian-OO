44862b5a3cc9af6870e2158d8021e917ddf18863
HandrianRivaldi74 <handrianr28@gmail.com>
2020-11-08 01:13:51 +0700

    Latihan40

---
+++ src/if2/pkg10119074/latihan40/warnakepribadian/oo/IF210119074Latihan40WarnaKepribadianOO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119074.latihan40.warnakepribadian.oo;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : HANDRIAN RIVALDI
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : 
+ */
+public class IF210119074Latihan40WarnaKepribadianOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKepribadian kepribadian = new WarnaKepribadian();
+        kepribadian.inputWarna();
+        kepribadian.inputNama();
+        kepribadian.cekKepribadian();
+    }
+    
+}
